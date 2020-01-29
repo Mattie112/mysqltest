7ba1add80d478815eb714baa0667f6d398dd6f1f
Matthijs van Schooten <matthijs@munisense.com>
2020-01-29 10:22:24 +0100

    Removed code that was not relevant to this issue

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mysqltest
-##How to test:
+## How to test:
 - docker-compose up
 - Open http://localhost/mysqltest.php and refresh 10 times
--- mysqltest.php
+++ mysqltest.php
@@ -1,19 +1,15 @@
 <?php
+
 $pdo_attr = [
     PDO::ATTR_PERSISTENT => true,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8;",
     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
 ];
-$database_host = "mysql";
-$database_name = "empty";
-$database_user = "root";
-$database_pwd = "iamreallysecure";
 
-$pdo = new PDO("mysql:host=" . $database_host . ";dbname=" . $database_name . ";charset=utf8", $database_user, $database_pwd, $pdo_attr);
+$pdo = new PDO("mysql:host=mysql;dbname=empty", "root", "iamreallysecure", $pdo_attr);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // The following will give a to many connections error after a few browser refreshes (connection limit is set to 10)
 var_dump($pdo->exec("SELECT 1;"));
+
 // While the following does NOT leave >10 connections open and continues to work
 //var_dump($pdo->query("SELECT 1;")->fetchAll());
-
